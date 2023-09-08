8d1efc7d55a1010aac2100940131903def908e74
phuctranj12 <140255613+phuctranj12@users.noreply.github.com>
2023-09-08 21:02:30 +0700

    Add files via upload

---
+++ bai2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("===================================\n");
+    printf("| STT| Ho Ten   | Tuoi| Gioi Tinh |\n");
+    printf("|01  | Nguyen A | 20  | Nam       |\n");
+    printf("|02  | Nguyen B | 22  | Nam       |\n");
+    printf("|03  | Nguyen C | 26  | Nam       |\n");
+
+}
---
+++ bai3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=8;
+    int b=9;
+    int c=10;
+    int d;
+    d = 10 * a * a + 5 * b * c + c * c;
+    printf("Gia tri cua d = %d",d);
+}
